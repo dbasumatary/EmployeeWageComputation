274d2297a790a55762e9e62a56489d9499cf30d0
dbasumatary <basudhondip@gmail.com>
2023-05-31 11:09:24 +0530

    Added: Welcome message

---
+++ EmpWageComputation/EmployeeWage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageComputation
+{
+    public class EmployeeWage
+    {
+        public void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome to employee wage computation");
+        }
+    }
+}
---
+++ EmpWageComputation/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace EmpWageComputation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeWage employeeWage = new EmployeeWage();
+            employeeWage.WelcomeMessage();
+        }
+    }
+}
